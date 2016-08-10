23dbd86c3326773dbcf4e5f5ecda2cdb8aca2c85
Irene Argollo Delgadillo <irene_airene@hotmail.com>
2016-08-09 20:23:26 -0400

    hechos del sistema

--- tacos.pl
+++ tacos.pl
@@ -1,8 +1,24 @@
 % Agente experto, encargador de cocinar tacos.
 
 % Hechos o funciones
+
+%%     preparacion de la crema de taco
 guisar(carne, sal).
 licuar(chile).
 licuar(cebolla).
+licuar(ajo).
+licuar(comino).
+mezclar(guiso_carne,licuado_verduras).
+
+%%	coccion de taco
+calentar(aceite).
+rellenar(tortilla, crema_taco).
+tostar(taco_crudo).
+
+%%	presentacion de taco
+servir(taco_cocido, letrucha).
+servir(taco_cocido, salsa).
+servir(taco_cocido, queso).
 
 % Reglas
+
