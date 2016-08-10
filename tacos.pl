fef29ee7401d1e47a4b70e345a2b5e08a00fcfa9
Irene Argollo Delgadillo <irene_airene@hotmail.com>
2016-08-09 23:40:18 -0400

    reglasparael proceso de hacer tacos

--- tacos.pl
+++ tacos.pl
@@ -1,6 +1,6 @@
 % Agente experto, encargador de cocinar tacos.
 
-% Hechos o funciones
+% HECHOS o funciones
 
 %%     preparacion de la crema de taco
 guisar(carne, sal).
@@ -9,16 +9,26 @@ licuar(cebolla).
 licuar(ajo).
 licuar(comino).
 mezclar(guiso_carne,licuado_verduras).
+mezclar(licuado_verduras, guiso_carne).
 
 %%	coccion de taco
 calentar(aceite).
 rellenar(tortilla, crema_taco).
+rellenar(crema_taco, tortilla).
 tostar(taco_crudo).
 
 %%	presentacion de taco
-servir(taco_cocido, letrucha).
-servir(taco_cocido, salsa).
-servir(taco_cocido, queso).
+servir(taco_tostado, lechuga).
+servir(taco_tostado, salsa).
+servir(taco_tostado, queso).
 
-% Reglas
+%%	orden
+fases(mezclar, rellenar, taco_servido).
+
+% REGLAS
+cremaTaco(X, Y) :-licuar(X);licuar(Y);guisar(Y, X), write('Se licuo la verdura').
+
+tostar_taco(X) :- calentar(X); tostar(X), write('se calento el aceite y termino de tostar el taco').
+
+servir_taco(X) :-servir(X,lechuga);servir(X, salsa); servir(X, queso), write('los tacos ya fueron servidos para consumirlos :) ').
 
